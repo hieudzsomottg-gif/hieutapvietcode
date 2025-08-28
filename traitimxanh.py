5933407c324fc0a50d4147f70967c13bd0b10160
hieuhoccode <hieudzsomottg@gmail.com>
2025-08-27 22:38:13 -0700

    Update traitimxanh.py

--- traitimxanh.py
+++ traitimxanh.py
@@ -1,130 +1,107 @@
 # hieutapvietcode
-hơi gà xíu
+#hơi gà xíu
 import random
 from math import sin, cos, pi, log
 from tkinter import *
 
-CANVAS_WIDTH = 640  
-CANVAS_HEIGHT = 480  
-CANVAS_CENTER_X = CANVAS_WIDTH / 2  
-CANVAS_CENTER_Y = CANVAS_HEIGHT / 2  
-IMAGE_ENLARGE = 11  
-HEART_COLOR = "#ff000"  #chỉnh màu ở đây
+CANVAS_WIDTH = 640
+CANVAS_HEIGHT = 480
+CANVAS_CENTER_X = CANVAS_WIDTH / 2
+CANVAS_CENTER_Y = CANVAS_HEIGHT / 2
+IMAGE_ENLARGE = 11
+HEART_COLOR = "#DB0DEE"
+
+def center_window(root, width, height):
+    screenwidth = root.winfo_screenwidth()
+    screenheight = root.winfo_screenheight()
+    size = f'{width}x{height}+{(screenwidth - width) // 2}+{(screenheight - height) // 2}'
+    root.geometry(size)
 
 def heart_function(t, shrink_ratio: float = IMAGE_ENLARGE):
-
-    x = 15 * (sin(t) ** 3)
-    y = -(13 * cos(t) - 4 * cos(2 * t) - 2 * cos(3 * t) - cos(3 * t))
-
+    x = 16 * (sin(t) ** 3)
+    y = -(13 * cos(t) - 5 * cos(2 * t) - 2 * cos(3 * t) - cos(4 * t))
     x *= shrink_ratio
     y *= shrink_ratio
-
- 
     x += CANVAS_CENTER_X
     y += CANVAS_CENTER_Y
-
     return int(x), int(y)
 
-
 def scatter_inside(x, y, beta=0.15):
-
-    ratio_x = - beta * log(random.random())
-    ratio_y = - beta * log(random.random())
-
+    ratio_x = -beta * log(random.random())
+    ratio_y = -beta * log(random.random())
     dx = ratio_x * (x - CANVAS_CENTER_X)
     dy = ratio_y * (y - CANVAS_CENTER_Y)
-
     return x - dx, y - dy
 
-
 def shrink(x, y, ratio):
- 
-    force = -1 / (((x - CANVAS_CENTER_X) ** 2 + (y - CANVAS_CENTER_Y) ** 2) )  
+    force = -1 / (((x - CANVAS_CENTER_X) ** 2 + (y - CANVAS_CENTER_Y) ** 2) ** 0.6)
     dx = ratio * force * (x - CANVAS_CENTER_X)
     dy = ratio * force * (y - CANVAS_CENTER_Y)
     return x - dx, y - dy
 
-
 def curve(p):
-  
-    return 2 * (2 * sin(3 * p)) / (2 * pi)
-
+    return 2 * (2 * sin(4 * p)) / (2 * pi)
 
 class Heart:
-  
-
     def __init__(self, generate_frame=20):
-        self._points = set()  
-        self._edge_diffusion_points = set()  
-        self._center_diffusion_points = set()  
-        self.all_points = {}  
+        self._points = set()
+        self._edge_diffusion_points = set()
+        self._center_diffusion_points = set()
+        self.all_points = {}
         self.build(2000)
-
         self.random_halo = 1000
-
         self.generate_frame = generate_frame
         for frame in range(generate_frame):
             self.calc(frame)
 
     def build(self, number):
-    
         for _ in range(number):
-            t = random.uniform(0, 2 * pi)  
+            t = random.uniform(0, 2 * pi)
             x, y = heart_function(t)
             self._points.add((x, y))
 
-       
         for _x, _y in list(self._points):
             for _ in range(3):
                 x, y = scatter_inside(_x, _y, 0.05)
-                self._edge_diffusion_points.add((x, y))
+                self._edge_diffusion_points.add((int(x), int(y)))
 
-    
         point_list = list(self._points)
-        for _ in range(10000):
+        for _ in range(4000):
             x, y = random.choice(point_list)
             x, y = scatter_inside(x, y, 0.17)
             self._center_diffusion_points.add((x, y))
 
     @staticmethod
     def calc_position(x, y, ratio):
-        
-        force = 1 / (((x - CANVAS_CENTER_X) ** 2 + (y - CANVAS_CENTER_Y) ** 2) ** 0.520) 
-
-        dx = ratio * force * (x - CANVAS_CENTER_X) + random.randint(-1, 1)
-        dy = ratio * force * (y - CANVAS_CENTER_Y) + random.randint(-1, 1)
-
+        force = 1 / (((x - CANVAS_CENTER_X) ** 2 + (y - CANVAS_CENTER_Y) ** 2) ** 0.520)
+        dx = ratio * force * (x - CANVAS_CENTER_X) + random.uniform(-1, 1)
+        dy = ratio * force * (y - CANVAS_CENTER_Y) + random.uniform(-1, 1)
         return x - dx, y - dy
 
     def calc(self, generate_frame):
-        ratio = 25 * curve(generate_frame / 10 * pi)  
-
+        ratio = 10 * curve(generate_frame / 10 * pi)
         halo_radius = int(4 + 6 * (1 + curve(generate_frame / 10 * pi)))
         halo_number = int(3000 + 4000 * abs(curve(generate_frame / 10 * pi) ** 2))
-
+        
         all_points = []
-
-      
-        heart_halo_point = set()  
+        
+        heart_halo_points = set()
         for _ in range(halo_number):
-            t = random.uniform(0, 2 * pi)  
-            x, y = heart_function(t, shrink_ratio=11.6)  
+            t = random.uniform(0, 2 * pi)
+            x, y = heart_function(t, shrink_ratio=11.6)
             x, y = shrink(x, y, halo_radius)
-            if (x, y) not in heart_halo_point:
-           
-                heart_halo_point.add((x, y))
+            if (x, y) not in heart_halo_points:
+                heart_halo_points.add((x, y))
                 x += random.randint(-14, 14)
                 y += random.randint(-14, 14)
                 size = random.choice((1, 2, 2))
-                all_points.append((x, y, size))
+                all_points.append((x, y, size, size))
 
-  
         for x, y in self._points:
             x, y = self.calc_position(x, y, ratio)
             size = random.randint(1, 3)
             all_points.append((x, y, size))
 
-      
         for x, y in self._edge_diffusion_points:
             x, y = self.calc_position(x, y, ratio)
             size = random.randint(1, 2)
@@ -138,20 +115,27 @@ class Heart:
         self.all_points[generate_frame] = all_points
 
     def render(self, render_canvas, render_frame):
-        for x, y, size in self.all_points[render_frame % self.generate_frame]:
-            render_canvas.create_rectangle(x, y, x + size, y + size, width=0, fill=HEART_COLOR)
-
+        if render_frame in self.all_points:
+            for point in self.all_points[render_frame]:
+                if len(point) == 4:
+                    x, y, w, h = point
+                    render_canvas.create_rectangle(x, y, x + w, y + h, width=0, fill=HEART_COLOR)
+                else:
+                    x, y, size = point
+                    render_canvas.create_rectangle(x, y, x + size, y + size, width=0, fill=HEART_COLOR)
 
 def draw(main: Tk, render_canvas: Canvas, render_heart: Heart, render_frame=0):
     render_canvas.delete('all')
     render_heart.render(render_canvas, render_frame)
-    main.after(160, draw, main, render_canvas, render_heart, render_frame + 1)
-
+    main.after(160, draw, main, render_canvas, render_heart, (render_frame + 1) % render_heart.generate_frame)
 
 if __name__ == '__main__':
-    root = Tk()  
+    root = Tk()
+    root.title("hieudz_heart")
+    center_window(root, CANVAS_WIDTH, CANVAS_HEIGHT)
     canvas = Canvas(root, bg='black', height=CANVAS_HEIGHT, width=CANVAS_WIDTH)
     canvas.pack()
-    heart = Heart()  
-    draw(root, canvas, heart)  
+    heart = Heart()
+    draw(root, canvas, heart)
+    Label(root, text="code từ hiếu đz", bg="black", fg="#0F82F5", font="Helvetica 20 bold").place(x=CANVAS_WIDTH // 2, y=CANVAS_HEIGHT - 30, anchor="s")
     root.mainloop()
